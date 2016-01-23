1e716852ec555e7b5489f21fee9177d4c22b9586
Gelidus <gelidus@flowup.eu>
2016-01-23 16:02:23 +0100

    [Models] Added two first model definitions

--- model/feedback.go
+++ model/feedback.go
@@ -1,1 +1,14 @@
 package model
+
+import "github.com/gophergala2016/gogen"
+
+// Feedback is model for the feedback entity
+var Feedback = &gogen.Model{
+	Name: "Feedback",
+	Fields: []gogen.Field{
+		{
+			Name: "Description",
+			Type: gogen.String,
+		},
+	},
+}
--- model/user.go
+++ model/user.go
@@ -1,1 +1,22 @@
 package model
+
+import "github.com/gophergala2016/gogen"
+
+// User is entity definition for the user
+var User = &gogen.Model{
+	Name: "User",
+	Fields: []gogen.Field{
+		{
+			Name: "Username",
+			Type: gogen.String,
+		},
+		{
+			Name: "Password",
+			Type: gogen.String,
+		},
+		{
+			Name: "Email",
+			Type: gogen.String,
+		},
+	},
+}
